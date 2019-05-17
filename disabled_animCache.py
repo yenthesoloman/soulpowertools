6e9db4c83706b5fb03494f02526b42f58aac41bb
yenthesoloman <50742384+yenthesoloman@users.noreply.github.com>
2019-05-17 11:18:25 -0400

    Update disabled_animCache.py

--- disabled_animCache.py
+++ disabled_animCache.py
@@ -1,3 +1,8 @@
 import maya.cmds as cmds
 
 sqList = cmds.ls('combined_*'+'*_INIT')
+sqList.remove('combined_GE01_INIT')
+cmds.select(sqList)
+
+for each in sqList:
+  cmds.setAttr('smplBs_'+each+'.envelope', 0)
